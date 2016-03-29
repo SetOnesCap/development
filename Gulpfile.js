11913fd1ec5540d0dd14e844441375ca52699884
Benjamin Dehli <superelg@gmail.com>
2016-03-29 22:48:01 +0200

    Add Node.js and Gulp

---
+++ Gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    jshint = require('gulp-jshint'),
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    concat = require('gulp-concat'),
+    del = require('del');
+
