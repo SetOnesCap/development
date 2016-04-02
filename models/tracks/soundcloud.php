7e3149205f9fece500b8c58f95de72b653dd0e48
Benjamin Dehli <superelg@gmail.com>
2016-04-02 18:14:15 +0200

    Add class for Soundcloud

---
+++ models/tracks/soundcloud.php
@@ -0,0 +1,27 @@
+<?php
+require_once($_SERVER['DOCUMENT_ROOT'] . '/environment.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/models/track.php');
+require_once($_SERVER['DOCUMENT_ROOT'] . '/services/audioCacher.php');
+
+class SoundcloudTrack extends Track
+{	
+	function __construct($data)
+	{
+		$title = $data->title;
+		$released_time_string = $data->release_day . "-" . $data->release_month . "-" . $data->release_year;
+		$released_time_unix = strtotime($released_time_string);
+		$released_time = date('M j, Y', $released_time_unix);
+		$playback_count = $data->playback_count;
+		$like_count = $data->favoritings_count;
+		$image_source = str_replace('large.jpg', 't500x500.jpg', $data->artwork_url);
+		$data_source = 'soundcloud';
+
+		$client_id = $_ENV["SOUNDCLOUD_CLIENT_ID"];
+
+		$streamUrl = $data->stream_url . '?client_id=' . $client_id;
+    	$local_audio_file = cacheAudio($streamUrl, 'mp3');
+
+
+		parent::__construct($title, $released_time, $released_time_unix, $playback_count, $like_count, $image_source, $data_source, $local_audio_file);
+	}	
+};
