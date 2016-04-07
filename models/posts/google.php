3dce99d867ea42b75a35a2fed20aa1115cf5eab0
Benjamin Dehli <superelg@gmail.com>
2016-04-07 23:15:39 +0200

    Improve image checker from Google API

--- models/posts/google.php
+++ models/posts/google.php
@@ -2,19 +2,30 @@
 
 class GooglePost extends Post
 {
+    function convertSafeimageUrl($string, $start, $end)
+    {
+        $string = " " . $string;
+        $ini = strpos($string, $start);
+        if ($ini == 0) return "";
+        $ini += strlen($start);
+        $len = strpos($string, $end, $ini) - $ini;
+        return urldecode(substr($string, $ini, $len));
+    }
+
     function getImage($data)
     {
         if (isset($data->attachments)) {
-            if (isset($data->attachments[0]->fullImage)){
-                return $data->attachments[0]->fullImage->url;
-            }
-            elseif (isset($data->attachments[0]->image)){
-                return $data->attachments[0]->image->url;
-            }
-            /*if (isset($data->attachments[0]->image)){
-                return $data->attachments[0]->image->url;
-            }*/
-        else{
+            if (isset($data->attachments[0]->fullImage)) {
+                $image_url = $data->attachments[0]->fullImage->url;
+                if (strpos($image_url, 'safe_image.php') !== false) {
+                    return $this->convertSafeimageUrl($image_url, "&url=", "&cfs=");
+                } elseif ($data->attachments[0]->fullImage->type == "image/jpeg") {
+                    $stripped_image_url = strtok($image_url, '?');
+                    return $stripped_image_url;
+                } else {
+                    return '';
+                }
+            } else {
                 return '';
             }
         } else {
@@ -33,7 +44,8 @@ class GooglePost extends Post
         $image_source = $this->getImage($data->object);
         $crop_image = false;
         $data_source = 'google';
-        parent::__construct($message, $created_time, $created_time_unix, $created_time_iso, $updated_time_iso, $image_source, $crop_image, $data_source);
+        $tags = '';
+        parent::__construct($message, $created_time, $created_time_unix, $created_time_iso, $updated_time_iso, $image_source, $crop_image, $tags, $data_source);
 
     }
 }
