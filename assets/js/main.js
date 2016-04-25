7ddbb551c1b9490493cd0d31f5886db6d159d298
Benjamin Dehli <superelg@gmail.com>
2016-04-25 22:02:47 +0200

    Update compiled scripts and stylesheets

--- assets/js/main.js
+++ assets/js/main.js
@@ -500,9 +500,29 @@ return ImagesLoaded;
 });
 
 
-$(window).load(function () {
+function updateMasonry() {
     var $container = $('.row').masonry();
     $container.imagesLoaded(function () {
         $container.masonry();
     });
+}
+$(window).load(function () {
+    updateMasonry();
+});
+
+$(".input-group input").change(function () {
+    console.log("change");
+    $(this).removeClass("is-not-empty");
+    if ($(this).val() == "") {
+    } else {
+        $(this).addClass("is-not-empty");
+    }
+});
+$(".sidenav-toggle").click(function () {
+    if ($("body").hasClass("sidebar-active")) {
+        $("body").removeClass("sidebar-active");
+    } else {
+        $("body").addClass("sidebar-active");
+    }
+    window.setTimeout(updateMasonry, 300);
 });
