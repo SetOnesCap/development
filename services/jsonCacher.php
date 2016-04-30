8956982c75dfad1c17c458ad2eadaf47af632ce7
Benjamin Dehli <superelg@gmail.com>
2016-04-30 19:24:29 +0200

    Add Html caching

--- services/jsonCacher.php
+++ services/jsonCacher.php
@@ -1,8 +1,8 @@
 <?php
 
-function cacheJson($json_data, $file_name)
+function cacheJson($json_data, $content_type, $file_name)
 {
-    $cache_directory = $_SERVER['DOCUMENT_ROOT'] . '/cache/json/';
+    $cache_directory = $_SERVER['DOCUMENT_ROOT'] . '/cache/' . $content_type . '/';
     if (!file_exists($cache_directory)) {
         mkdir($cache_directory, 0777, true);
     }
@@ -15,13 +15,31 @@ function cacheJson($json_data, $file_name)
 function cacheJsonFromUrl($url, $file_name)
 {
     $json_data = file_get_contents($url);
-    cacheJson($json_data, $file_name);
+    cacheJson($json_data, 'json', $file_name);
+    return $json_data;
+}
+
+function cacheHtmlFromUrl($url, $file_name)
+{
+    $json_data = file_get_contents($url);
+    cacheJson($json_data, 'html', $file_name);
     return $json_data;
 }
 
 function getJson($file_name)
 {
     $cacheFile = $_SERVER['DOCUMENT_ROOT'] . '/cache/json/' . $file_name;
+    if (file_exists($cacheFile)) {
+        $fh = fopen($cacheFile, 'r');
+        return fread($fh, filesize($cacheFile));
+    } else {
+        return "No data available";
+    }
+}
+
+function getHtml($file_name)
+{
+    $cacheFile = $_SERVER['DOCUMENT_ROOT'] . '/cache/html/' . $file_name;
     if (file_exists($cacheFile)) {
         $fh = fopen($cacheFile, 'r');
         return fread($fh, filesize($cacheFile));
