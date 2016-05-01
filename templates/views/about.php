2aa6b213ad6a54b9807fdf462902961ae064f3bc
Benjamin Dehli <superelg@gmail.com>
2016-05-01 20:50:55 +0200

    Add content to view templates

--- templates/views/about.php
+++ templates/views/about.php
@@ -2,7 +2,24 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');
 ?>
     <div class="container">
-        <h1>About</h1>
+        <div class="row">
+            <div class='col-lg-12'>
+                <div itemscope itemtype="http://schema.org/NewsArticle" class='box z-1'>
+                    <div class='content'>
+                        <h1 class="sr-only">About</h1>
+                        <p>In their short time together they have secured airtime on Norwegian radio stations, like NRK P3, and concerts at well-known venues like Rockefeller and John Dee.
+                        </p>
+                        <p> With music you fall in love with immediately, it is easy for the audience to get carried away. The band consists of six members who clearly bring their individuality into the group, giving the band a very diverse and exciting sound that stems from both pop, rock, jazz and blues.
+                        </p>
+                        <p> In February the band released their third single, "Don’t you wanna know?", The song is produced by Christer-André Cederberg and mastered by Tom Coyne (Taylor Swift, Adele, Justin Bieber, The Weekend).
+                        </p>
+                        <p> The song is about catching the attention of that special someone who you really want to get that attention from. Therefore, the whole song, all the instrumentals and sounds, not just text, are composed in a way that makes you pay attention, just like a stranger on the street who winks at you and stops you in your tracks, according to the band itself.
+                        </p>
+                        <p> The band, which was formed in the fall of 2014 have in other words accomplished a lot in a short amount of time. Concerts, festivals and, last but not least, a music video recording of the single "Like a Stone". Set One's Cap is a band that one can safely say gives a 110% to their music, and there is still many exciting things in store for this dedicated band.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php');
--- templates/views/music.php
+++ templates/views/music.php
@@ -3,7 +3,8 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');
 ?>
 <div class="container">
     <h1>Music</h1>
-    <div class='row'>
+    <div class='row masonry-row'>
+        <div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 masonry-item grid-sizer'></div>
         <?php
         foreach ($tracks as $track) {
             getTrack($track);
