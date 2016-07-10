d419561f4ce984e5f8f32e9547da253b841bfbe2
Benjamin Dehli <superelg@gmail.com>
2016-07-11 00:47:36 +0200

    Implement simple-gui-tempalte on News view

--- templates/views/news.php
+++ templates/views/news.php
@@ -2,8 +2,7 @@
 include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');
 ?>
 
-    <div class="container">
-
+    <div class="content">
         <h1 class="sr-only">News</h1>
         <div class='row masonry-row'>
             <div class='col-xs-12 col-sm-6 col-md-4 col-lg-3 masonry-item grid-sizer'></div>
@@ -13,13 +12,13 @@ include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/header.php');
             } ?>
         </div>
     </div>
-<div class="action-button"></div>
-<div class="action-menu">
-    <button class="toggle-action-button facebook" value="facebook"><span class="icon"></span></button>
-    <button class="toggle-action-button twitter" value="twitter"><span class="icon"></span></button>
-    <button class="toggle-action-button instagram" value="instagram"><span class="icon"></span></button>
-    <button class="toggle-action-button google" value="google"><span class="icon"></span></button>
-</div>
+    <div class="action-button"></div>
+    <div id="action-menu" class="action-menu">
+        <button class="toggle-action-button facebook" value="facebook"><span class="icon"></span></button>
+        <button class="toggle-action-button twitter" value="twitter"><span class="icon"></span></button>
+        <button class="toggle-action-button instagram" value="instagram"><span class="icon"></span></button>
+        <button class="toggle-action-button google" value="google"><span class="icon"></span></button>
+    </div>
 
 <?php
 include_once($_SERVER['DOCUMENT_ROOT'] . '/templates/footer.php');
